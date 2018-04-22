4eabe4753ee88166d6fc659ec96b7348ed0db3ec
Daniel Ferguson <danielferguson@me.com>
2018-04-22 15:58:24 +0100

    Extract event handling logic
    By pulling the event handling out of the run() routine:
     * run() becomes more readable
     * event handling becomes easier to reason about (to some extent)

--- src/bin/hi.rs
+++ src/bin/hi.rs
@@ -18,6 +18,141 @@ use hi::command_prompt::{CommandMachineEvent, CommandPrompt};
 use hi::screen::Screen;
 use hi::{byte_display, status_bar, Frame, State};
 
+struct Context<'a> {
+    file_path: &'a str,
+}
+
+enum HandlerStatus {
+    Continue,
+    Quit,
+}
+
+struct EventHandler<'a, 'b, T: 'a>
+where
+    T: Write,
+{
+    out: &'a mut T,
+    prompt: CommandPrompt,
+    screen: &'b mut Screen<'b>,
+}
+
+impl<'a, 'b, T: 'a> EventHandler<'a, 'b, T>
+where
+    T: Write,
+{
+    fn new(out: &'a mut T, screen: &'b mut Screen<'b>) -> Self {
+        let prompt = CommandPrompt::new();
+        Self {
+            out,
+            prompt,
+            screen,
+        }
+    }
+
+    fn call(
+        &mut self,
+        context: &Context,
+        event: termion::event::Event,
+    ) -> Result<HandlerStatus, Box<StdError>> {
+        let screen = &mut self.screen;
+        screen.clear_dirty_flags();
+        let mut command_bar_focus = false;
+        let mut stdout = &mut self.out;
+
+        match screen.state {
+            State::Wait => match event {
+                Event::Key(Key::Char('q')) => {
+                    write!(stdout, "{}", termion::cursor::Goto(1, 1))?;
+                    return Ok(HandlerStatus::Quit);
+                }
+                Event::Key(Key::Char('h')) => screen.scroll_left(),
+                Event::Key(Key::Char('l')) => screen.scroll_right(),
+                Event::Key(Key::Char('j')) => screen.down(),
+                Event::Key(Key::Char('k')) => screen.up(),
+                Event::Key(Key::Char(':')) => {
+                    screen.prompt();
+                    command_bar_focus = true;
+                }
+                Event::Key(Key::Ctrl('d')) | Event::Key(Key::PageDown) => screen.page_down(),
+                Event::Key(Key::Ctrl('u')) | Event::Key(Key::PageUp) => screen.page_up(),
+                Event::Key(Key::Home) => screen.start(),
+                Event::Key(Key::End) => screen.end(),
+                _ => {}
+            },
+            State::Prompt => match event {
+                Event::Key(x) => {
+                    use hi::command_prompt::Command::{ScrollX, ScrollY, SetOffset, SetWidth};
+
+                    self.prompt = self.prompt.step(x);
+
+                    match self.prompt.last_event {
+                        CommandMachineEvent::Reset | CommandMachineEvent::UnknownCommand(..) => {
+                            screen.reset_prompt()
+                        }
+                        CommandMachineEvent::Update => screen.update_prompt(),
+                        CommandMachineEvent::Execute(SetWidth(n)) => screen.set_width(n),
+                        CommandMachineEvent::Execute(SetOffset(n)) => screen.set_offset(n),
+                        CommandMachineEvent::Execute(ScrollX(n)) => screen.set_scroll_x(n),
+                        CommandMachineEvent::Execute(ScrollY(n)) => screen.set_scroll_y(n),
+                    }
+                }
+                e => {
+                    let message = format!("{:?}", e);
+                    info!("{}", message);
+                }
+            },
+        }
+
+        if screen.data_frame_dirty {
+            let len = screen.data.len();
+            let data = &screen.data[screen.offset..len];
+            byte_display::render(&mut stdout, data, &screen);
+        }
+
+        if screen.status_bar_dirty {
+            status_bar::render(&mut stdout, &screen, context.file_path);
+        }
+
+        use CommandMachineEvent::{Execute, Reset, Update};
+        if screen.prompt_bar_dirty {
+            match self.prompt.last_event {
+                Reset | Execute(_) | CommandMachineEvent::UnknownCommand(_) => {
+                    write!(
+                        stdout,
+                        "{}{}{}",
+                        termion::cursor::Goto(1, screen.frame.height),
+                        termion::clear::CurrentLine,
+                        termion::cursor::Hide
+                    )?;
+                }
+                Update => {
+                    write!(
+                        stdout,
+                        "{}{}{}:{}",
+                        termion::cursor::Show,
+                        termion::cursor::Goto(1, screen.frame.height),
+                        termion::clear::CurrentLine,
+                        self.prompt.text,
+                    )?;
+                }
+            }
+        }
+
+        if command_bar_focus {
+            write!(
+                stdout,
+                "{}{}{}:",
+                termion::cursor::Show,
+                termion::cursor::Goto(1, screen.frame.height),
+                termion::clear::CurrentLine,
+            )?;
+        }
+
+        stdout.flush()?;
+        Ok(HandlerStatus::Continue)
+    }
+}
+
 fn run() -> Result<(), Box<StdError>> {
     env_logger::init()?;
     let path = env::args().nth(1).expect("Usage: hi FILE");
@@ -46,101 +181,18 @@ fn run() -> Result<(), Box<StdError>> {
     status_bar::render(&mut stdout, &screen, &path);
     stdout.flush()?;
 
-    let mut command_machine = CommandPrompt::new();
+    let context = Context { file_path: &path };
 
-    for evt in stdin.events() {
-        screen.clear_dirty_flags();
-        let evt = evt?;
-        let mut command_bar_focus = false;
-
-        match screen.state {
-            State::Wait => match evt {
-                Event::Key(Key::Char('q')) => {
-                    write!(stdout, "{}", termion::cursor::Goto(1, 1))?;
-                    break;
-                }
-                Event::Key(Key::Char('h')) => screen.scroll_left(),
-                Event::Key(Key::Char('l')) => screen.scroll_right(),
-                Event::Key(Key::Char('j')) => screen.down(),
-                Event::Key(Key::Char('k')) => screen.up(),
-                Event::Key(Key::Char(':')) => {
-                    screen.prompt();
-                    command_bar_focus = true;
-                }
-                Event::Key(Key::Ctrl('d')) | Event::Key(Key::PageDown) => screen.page_down(),
-                Event::Key(Key::Ctrl('u')) | Event::Key(Key::PageUp) => screen.page_up(),
-                Event::Key(Key::Home) => screen.start(),
-                Event::Key(Key::End) => screen.end(),
-                _ => {}
-            },
-            State::Prompt => match evt {
-                Event::Key(x) => {
-                    use hi::command_prompt::Command::{ScrollX, ScrollY, SetOffset, SetWidth};
-                    command_machine = command_machine.step(x);
-                    match command_machine.last_event {
-                        CommandMachineEvent::Reset | CommandMachineEvent::UnknownCommand(..) => {
-                            screen.reset_prompt()
-                        }
-                        CommandMachineEvent::Update => screen.update_prompt(),
-                        CommandMachineEvent::Execute(SetWidth(n)) => screen.set_width(n),
-                        CommandMachineEvent::Execute(SetOffset(n)) => screen.set_offset(n),
-                        CommandMachineEvent::Execute(ScrollX(n)) => screen.set_scroll_x(n),
-                        CommandMachineEvent::Execute(ScrollY(n)) => screen.set_scroll_y(n),
-                    }
-                }
-                e => {
-                    let message = format!("{:?}", e);
-                    info!("{}", message);
-                }
-            },
+    {
+        // constrain stdout mutable borrow to event handling loop
+        let mut handler = EventHandler::new(&mut stdout, &mut screen);
+        for event in stdin.events() {
+            let event = event?;
+            match handler.call(&context, event)? {
+                HandlerStatus::Continue => {}
+                HandlerStatus::Quit => break,
+            };
         }
-
-        if screen.data_frame_dirty {
-            let len = screen.data.len();
-            let data = &screen.data[screen.offset..len];
-            byte_display::render(&mut stdout, data, &screen);
-        }
-
-        if screen.status_bar_dirty {
-            status_bar::render(&mut stdout, &screen, &path);
-        }
-
-        use CommandMachineEvent::{Execute, Reset, Update};
-        if screen.prompt_bar_dirty {
-            match command_machine.last_event {
-                Reset | Execute(_) | CommandMachineEvent::UnknownCommand(_) => {
-                    write!(
-                        stdout,
-                        "{}{}{}",
-                        termion::cursor::Goto(1, screen.frame.height),
-                        termion::clear::CurrentLine,
-                        termion::cursor::Hide
-                    )?;
-                }
-                Update => {
-                    write!(
-                        stdout,
-                        "{}{}{}:{}",
-                        termion::cursor::Show,
-                        termion::cursor::Goto(1, screen.frame.height),
-                        termion::clear::CurrentLine,
-                        command_machine.text,
-                    )?;
-                }
-            }
-        }
-
-        if command_bar_focus {
-            write!(
-                stdout,
-                "{}{}{}:",
-                termion::cursor::Show,
-                termion::cursor::Goto(1, screen.frame.height),
-                termion::clear::CurrentLine,
-            )?;
-        }
-
-        stdout.flush()?;
     }
 
     write!(stdout, "{}", termion::cursor::Show)?;
